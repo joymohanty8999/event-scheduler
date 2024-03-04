080cb35dd31d71ed26658995b1c982a0e11b990a
Joseph Mohanty <josephmohanty@josephs-mbp-3.dyndns.rice.edu>
2024-03-04 12:20:52 -0600

    Adding data persistance by loading and saving to events.json

--- main.py
+++ main.py
@@ -1,27 +1,32 @@
+import json
 from datetime import datetime
 
 class MyEventScheduler:
 
-    def __init__(self):
+    def __init__(self, events_file = 'events.json'):
 
         self.events = [] #list to add, display and remove events from
+        self.events_file = events_file
+        self.load_events()
 
     def add_event(self, title, date, description=""):
 
         # Check format of data and its uniqueness
         if not self.check_valid_date(date) or self.event_title_exists(title):
-            return False
+            return False, "Invalid date format, or event already exists"
         
         self.events.append({"title": title, "date": date, "description": description})
+        self.save_events()
 
-        return True
+        return True, "Addition of event is successful"
 
     def remove_event(self, title):
 
         for event in self.events:
             if event["title"] == title:
                 self.events.remove(event)
-                return True
+                self.save_events()
+                return True, "Deletion of event is successful"
             
         return False
 
@@ -32,6 +37,21 @@ class MyEventScheduler:
 
         for event in self.events:
             print(f"Title: {event['title']}, Date: {event['date']}, Description: {event['description']}")
+
+    def save_events(self):
+
+        with open(self.events_file, 'w') as file:
+            json.dump(self.events, file)
+
+
+    def load_events(self):
+
+        try:
+            with open(self.events_file, 'r') as file:
+                self.events = json.load(file)
+
+        except FileNotFoundError:
+            self.events = []
 
     def check_valid_date(self, date):
 
@@ -45,6 +65,6 @@ class MyEventScheduler:
         return any(event["title"] == title for event in self.events)
 
 scheduler = MyEventScheduler()
-scheduler.add_event("Event_1", "2024-03-10", "This is a sample event.")
-scheduler.add_event("Event_2", "2024-03-15") 
+scheduler.add_event("COMP 539 Presentation", "2024-03-10", "Present your Pitch to the class")
+scheduler.add_event("COMP 646 Class", "2024-03-15") 
 scheduler.get_events()
